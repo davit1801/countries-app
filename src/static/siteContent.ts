efdc75fc86e06499333026b90b30ef317bf0508b
davit1801 <155184127+davit1801@users.noreply.github.com>
2026-03-11 21:10:05 +0400

    test 2

--- src/static/siteContent.ts
+++ src/static/siteContent.ts
@@ -89,7 +89,7 @@ const CONTENT: ContentMap = {
       { name: 'ჩვენს შესახებ', path: 'about' },
       { name: 'დაგვიკავშირდით', path: 'contact' },
     ],
-    heroHeading: 'ჩვენ ვპოულობთ საუკეთესო ტურებს თქვენთვისss',
+    heroHeading: 'ჩვენ ვპოულობთ საუკეთესო ტურებს თქვენთვისsss',
     countriesHeading: 'პოპულარული ქვეყნები',
     copyrightTitle: 'ყველა უფლება დაცულია © 2024',
     countrySort: {
